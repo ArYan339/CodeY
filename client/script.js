4d91f70326a5639eec4684be3b70eb61c728f455
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2023-04-22 11:15:33 -0500

    Updated the server URL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-nx0n.onrender.com', {
+    const response = await fetch('https://codey-nct1.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',

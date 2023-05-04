c2e2f7cbd967e4e1f8d3d5f35ea4a18117ba0f10
ArYan Si <117717585+ArYan339@users.noreply.github.com>
2023-05-04 09:52:29 -0500

    Update the index file

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codey-9ay8.onrender.com', {
+    const response = await fetch('https://codey-vjyw.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
